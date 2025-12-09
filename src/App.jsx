6c71fdec9a12f4b27a8fe442891980d6c4484826
FengFengovo <2083147939@qq.com>
2025-12-09 10:11:36 +0800

    style:移除边距

--- src/App.jsx
+++ src/App.jsx
@@ -202,7 +202,7 @@ function App() {
   }
 
   return (
-    <div className="min-h-screen w-full flex items-center justify-center p-2 sm:p-4">
+    <div className="min-h-screen w-full flex items-center justify-center sm">
       {view === 'home' && (
         <HomePage
           userName={userName}
